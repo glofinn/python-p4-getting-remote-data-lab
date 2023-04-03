0b99a727f35be1417a65a3bdaaff8f6289564a21
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-04-03 16:22:09 -0400

    complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        programs_list = []
+        programs = json.loads(self.get_response_body())
+        for program in programs:
+            programs_list.append(program)
+
+        return programs_list
